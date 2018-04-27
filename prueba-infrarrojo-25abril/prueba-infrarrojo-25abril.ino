d0ff41ccc342e3eb3b2e9ee109ad93011fd899c4
marcoacasas <marcoacasasm@gmail.com>
2018-04-27 11:59:32 -0500

    Codigo de infrarrojo ya sirviendo.

--- prueba-infrarrojo-25abril/prueba-infrarrojo-25abril.ino
+++ prueba-infrarrojo-25abril/prueba-infrarrojo-25abril.ino
@@ -3,18 +3,18 @@ int a, b, c;
 void setup() {
   // put your setup code here, to run once:
   pinMode(2,INPUT);
-  pinMode(2,INPUT);
-  pinMode(2,INPUT);
+  pinMode(3,INPUT);
+  pinMode(A0,INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  a.digitalRead(2);
-  a.digitalRead(2);
-  a.digitalRead(2);
+  a = digitalRead(2);
+  b = digitalRead(3);
+  c = analogRead(A0);
   Serial.print(a);
-  Serial.print(b);
-  Serial.print(c);
+  Serial.println(b);
+  Serial.println(c);
   delay(100);
 }
